385a08135d8629be858f3039c904f3482e0f7196
Edixon Piña <edixonalbertto@gmail.com>
2022-11-24 23:49:14 -0400

    frontend: add styles to responsive mobile
    - add in template footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -98,8 +98,8 @@ async function sendMail(mailBody: TMail): Promise<boolean> {
           </button>
         </form>
 
-        <div class="output-container">
-          <div class="output" v-highlight>
+        <div class="output">
+          <div class="code-highlight" v-highlight>
             <pre class="language-javascript">
 <code>{{responseCode}}</code>
             </pre>
@@ -107,6 +107,14 @@ async function sendMail(mailBody: TMail): Promise<boolean> {
         </div>
       </div>
     </main>
+
+    <footer>
+      <a href="https://github.com/EdixonAlberto/challenge-chiper">
+        <!-- prettier-ignore -->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
+        <span>Created by Edixon 🍍</span>
+      </a>
+    </footer>
   </div>
 </template>
 
@@ -125,8 +133,10 @@ $color-grey-dark: #75828e;
   padding-right: 140px;
 
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  row-gap: 10px;
 
   background: $color-background;
 
@@ -282,7 +292,7 @@ $color-grey-dark: #75828e;
         }
       }
 
-      .output-container {
+      .output {
         width: 50%;
         height: 100%;
         padding: 20px;
@@ -291,13 +301,31 @@ $color-grey-dark: #75828e;
         border-radius: 4px;
         box-shadow: 0px 5px 20px -7px $color-grey-dark;
 
-        .output,
+        .code-highlight,
         pre {
           height: 100%;
           margin: 0;
           overflow-y: hidden;
           overflow-x: auto;
         }
+      }
+    }
+  }
+
+  footer {
+    height: 30px;
+    margin-left: auto;
+
+    a {
+      display: flex;
+      justify-content: center;
+      column-gap: 5px;
+
+      color: $color-grey-dark;
+      text-decoration: none;
+
+      svg {
+        width: 20px;
       }
     }
   }
@@ -328,7 +356,60 @@ $color-grey-dark: #75828e;
 // Mobile
 @media screen and (max-width: 576px) {
   .app {
-    // TODO: add code responsive to mobile
+    height: 100%;
+    padding: 10px 30px;
+
+    main {
+      .content-header {
+        padding: 0;
+
+        h2 {
+          font-size: 20px;
+        }
+
+        p {
+          padding: 0 20px;
+          font-size: 14px;
+        }
+      }
+
+      .content-main {
+        height: 100%;
+        flex-direction: column;
+        row-gap: 20px;
+
+        form,
+        .output {
+          width: 100%;
+          padding: 20px;
+        }
+
+        form {
+          .ch-input {
+            label,
+            input,
+            select,
+            textarea {
+              font-size: 14px;
+            }
+          }
+
+          button {
+            span {
+              font-size: 16px;
+            }
+
+            &.loading::before {
+              right: 40px;
+            }
+          }
+        }
+
+        .output {
+          height: max-content;
+        }
+      }
+    }
   }
 }
 </style>
