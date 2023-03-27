2cc28854bd2b52b498cd9de75cc23db704978327
Edixon Piña <edixonalbertto@gmail.com>
2023-03-27 02:09:52 -0400

    Update vite.config.ts
    fix error: change base url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/challenge-chiper/',
+  base: '/service-email-tolerant/',
   plugins: [vue()],
   server: {
     host: 'localhost',
